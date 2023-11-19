79b0949bee2411e24addb2dbe6d6d4babe34d7d5
Illyakravchuk <ilakrawchyk18@gmail.com>
2023-11-19 20:42:18 +0200

    updated the main() function
    Used all the necessary functions for the program to work.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-async function main() {
+const { parseLine } = require('./src/parseLine')
+const { Calculate } = require('./src/utilityFunctions')
+const writeFile = require('./src/writeFile')
 
+async function main() {
+    const data = await parseLine('input.txt')
+    let result = Calculate(data)
+    console.log(writeFile('output.txt', result))
 }
 main()
